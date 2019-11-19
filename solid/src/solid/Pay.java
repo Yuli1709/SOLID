73df0db453c670837b342d213409d16571dde7b5
CltControl <CltControl@GGIECSOFWRK135.espol.edu.ec>
2019-11-19 13:33:48 -0500

    cambio 2

---
+++ solid/src/solid/Pay.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solid;
+
+/**
+ *
+ * @author CltControl
+ */
+public interface Pay {
+    public void pay(String method, Book book);
+}
---
+++ solid/src/solid/PayCard.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solid;
+
+/**
+ *
+ * @author CltControl
+ */
+public class PayCard implements Pay{
+
+    @Override
+    public void pay(String method, Book book) {
+        /*
+        * change state to true
+        */
+    }
+    
+}
